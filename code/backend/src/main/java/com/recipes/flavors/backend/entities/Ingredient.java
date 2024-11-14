5141c8d149356a2241c770eb21b5c13dd0eeb843
raphael sena <rsenares1@gmail.com>
2024-11-14 16:50:21 -0300

    perf: update the imports of all classes that was needed

--- code/backend/src/main/java/com/recipes/flavors/backend/entities/Ingredient.java
+++ code/backend/src/main/java/com/recipes/flavors/backend/entities/Ingredient.java
@@ -1,7 +1,17 @@
 package com.recipes.flavors.backend.entities;
 
 import com.recipes.flavors.backend.entities.enums.UnitEnum;
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
--- code/backend/src/main/java/com/recipes/flavors/backend/entities/User.java
+++ code/backend/src/main/java/com/recipes/flavors/backend/entities/User.java
@@ -1,7 +1,15 @@
 package com.recipes.flavors.backend.entities;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import jakarta.persistence.*;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
