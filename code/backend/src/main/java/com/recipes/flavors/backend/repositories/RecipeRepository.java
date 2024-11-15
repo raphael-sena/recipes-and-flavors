daa159719aa1f71becea7d50808eb274634dab20
raphael sena <rsenares1@gmail.com>
2024-11-15 17:52:54 -0300

    perf: include specific imports

--- code/backend/src/main/java/com/recipes/flavors/backend/repositories/RecipeRepository.java
+++ code/backend/src/main/java/com/recipes/flavors/backend/repositories/RecipeRepository.java
@@ -2,6 +2,8 @@ package com.recipes.flavors.backend.repositories;
 
 import com.recipes.flavors.backend.entities.Recipe;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface RecipeRepository extends JpaRepository<Recipe, Long> {
 }
--- code/backend/src/main/java/com/recipes/flavors/backend/repositories/ReviewRepository.java
+++ code/backend/src/main/java/com/recipes/flavors/backend/repositories/ReviewRepository.java
@@ -2,6 +2,8 @@ package com.recipes.flavors.backend.repositories;
 
 import com.recipes.flavors.backend.entities.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ReviewRepository extends JpaRepository<Review, Long> {
 }
