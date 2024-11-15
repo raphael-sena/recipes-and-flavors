85670c0e7d2d210bada03db69c049425bbaca6b4
raphael sena <rsenares1@gmail.com>
2024-11-15 14:52:06 -0300

    feat: add password encoder and create ProfileEnum

--- code/backend/src/main/java/com/recipes/flavors/backend/entities/User.java
+++ code/backend/src/main/java/com/recipes/flavors/backend/entities/User.java
@@ -2,15 +2,8 @@ package com.recipes.flavors.backend.entities;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.Table;
+import com.recipes.flavors.backend.entities.enums.ProfileEnum;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -19,6 +12,7 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 @Entity
 @Table(name = "tb_user")
@@ -51,6 +45,22 @@ public class User {
     private Set<Review> reviews = new HashSet<>();
 
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.EAGER)
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     @JsonIgnoreProperties({"user", "user", "ingredients", "methods", "preparationTime", "cookTime", "totalTime", "servings", "dietType", "difficulty", "reviews"})
     private Set<Recipe> recipes = new HashSet<>();
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    @CollectionTable(name = "user_profile")
+    @Column(name = "profile", nullable = false)
+    private Set<Integer> profiles = new HashSet<>();
+
+    public Set<ProfileEnum> getprofiles() {
+        return this.profiles.stream()
+                .map(x -> ProfileEnum.toEnum(x))
+                .collect(Collectors.toSet());
+    }
+    public void addProfile(ProfileEnum profileEnum) {
+        this.profiles.add(profileEnum.getCode());
+    }
 }
--- code/backend/src/main/java/com/recipes/flavors/backend/services/UserService.java
+++ code/backend/src/main/java/com/recipes/flavors/backend/services/UserService.java
@@ -3,20 +3,27 @@ package com.recipes.flavors.backend.services;
 import com.recipes.flavors.backend.entities.User;
 import com.recipes.flavors.backend.entities.dto.user.UserCreateDTO;
 import com.recipes.flavors.backend.entities.dto.user.UserUpdateDTO;
+import com.recipes.flavors.backend.entities.enums.ProfileEnum;
 import com.recipes.flavors.backend.repositories.UserRepository;
 import com.recipes.flavors.backend.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.validation.Valid;
 import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Service
 public class UserService {
 
     @Autowired
     private UserRepository userRepository;
+
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
 
     public User findById(Long id) {
         Optional<User> user = this.userRepository.findById(id);
@@ -29,7 +36,10 @@ public class UserService {
         obj.setId(null);
         obj.setName(obj.getName());
         obj.setEmail(obj.getEmail());
-        obj.setPassword(obj.getPassword());
+        obj.setPassword(this.bCryptPasswordEncoder.encode(obj.getPassword()));
+        obj.setProfiles(Stream.
+                of(ProfileEnum.USER.getCode())
+                .collect(Collectors.toSet()));
         obj = this.userRepository.save(obj);
         return obj;
     }
@@ -37,7 +47,7 @@ public class UserService {
     @Transactional
     public User update(User obj) {
         User newObj = findById(obj.getId());
-        newObj.setPassword(obj.getPassword());
+        newObj.setPassword(this.bCryptPasswordEncoder.encode(obj.getPassword()));
         return this.userRepository.save(newObj);
     }
 
