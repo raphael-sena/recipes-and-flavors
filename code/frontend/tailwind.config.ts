bf54a9975f9e5f1324b1c0c664531844f5a05777
raphael sena <rsenares1@gmail.com>
2024-11-15 13:50:19 -0300

    feat: color config

--- code/frontend/tailwind.config.ts
+++ code/frontend/tailwind.config.ts
@@ -9,8 +9,15 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        background: "#E1DEC5",
+        light: "#E1DEC5",
+        darkBrown: "#60452E", 
+        lightBrown: "rgba(96, 69, 46, 0.5",
+        darkBlue: "rgba(9, 40, 34)",
+        lightBlue: "rgba(9, 40, 34, 0.8)",
+        darkRed: "rgba(227, 34, 34)",
+        lightRed: "rgba(227, 34, 34, 0.5)",
+        yellow: "#AFBD18",
       },
     },
   },
